f46e82a9726e4960d97b94c5960d017218af6111
Fabio Zendhi Nagao <nagaozen@hotmail.com>
2018-04-25 12:41:12 -0300

    replacing `nomnom` with `commander`
    and also `util.puts` with `console.log`

--- dist/cli.js
+++ dist/cli.js
@@ -1,67 +1,52 @@
 #!/usr/bin/env node
 
-var util = require('util');
 var fs = require("fs");
 var parserParse = require("./orderly").parse;
-var options = require("nomnom")
-  .scriptName("orderly")
-  .opts({
-    file: {
-      position: 0,
-      help: "file to parse; otherwise uses stdin"
-    },
-    version: {
-      string: '-v, --version',
-      help: 'print version and exit',
-      callback: function() {
-        return JSON.parse(fs.readFileSync(__dirname + "/../package.json", "utf8")).version;
-      }
-    },
-    outfile : {
-      string: '-o FILE, --output-file FILE',
-      help: 'write output to the file'
-    },
-    indent : {
-      string: '-t CHAR, --indent CHAR',
-      default: "  ",
-      help: 'character(s) to use for indentation'
-    }
-  })
-  .parseArgs();
+var options = require("commander");
 
+options
+	.version( JSON.parse(fs.readFileSync(__dirname + "/../package.json", "utf8")).version )
+	.arguments("<file>")
+	.action(function (file) {
+		options.file = file;
+	})
+	.option("-o <FILE>, --output-file <FILE>", "write output to the file")
+	.option("-t [CHAR], --indent [CHAR]", "character(s) to use for indentation", /^\s+$/i, "  ")
+	
+options.parse(process.argv);	
 
-function parse (source) {
-  try {
-    var parsed = parserParse(source);
-    return JSON.stringify(parsed,null,options.indent);
-  } catch (e) {
-    util.puts(e);
-    process.exit(1);
-  }
+function parse(source) {
+	try {
+		var parsed = parserParse(source);
+		return JSON.stringify(parsed, null, options.indent);
+	} catch (e) {
+		console.log(e);
+		process.exit(1);
+	}
 }
 
-function main (args) {
-  var source = '';
-  var p = require('path');
-  if (options.file) {
-    var path = p.resolve(options.file);
-    source = parse(fs.readFileSync(path, "utf8"));
-    if (options.outfile) {
-      fs.writeSync(fs.openSync(p.resolve(options.outfile),'w+'), source, 0, "utf8");
-    } else {
-      util.puts(source);
-    }
-  } else {
-    var stdin = process.openStdin();
-    stdin.setEncoding('utf8');
+function main(args) {
+	var source = '';
+	var p = require('path');
+	if (options.file) {
+		var path = p.resolve(options.file);
+		source = parse(fs.readFileSync(path, "utf8"));
+		if (options.outfile) {
+			fs.writeSync(fs.openSync(p.resolve(options.outfile), 'w+'), source, 0, "utf8");
+		} else {
+			console.log(source);
+		}
+	} else {
+		var stdin = process.openStdin();
+		stdin.setEncoding('utf8');
 
-    stdin.on('data', function (chunk) {
-      source += chunk.toString('utf8');
-    });
-    stdin.on('end', function () {
-      util.puts(parse(source));
-    });
-  }
+		stdin.on('data', function(chunk) {
+			source += chunk.toString('utf8');
+		});
+		stdin.on('end', function() {
+			console.log(parse(source));
+		});
+	}
 }
 
 main(process.argv.slice(1));
